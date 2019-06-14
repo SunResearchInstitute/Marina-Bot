29b859e6db1208cbd7b80ac40fd56162fd68fdff
Sun <22406854+SunTheCourier@users.noreply.github.com>
2019-06-14 11:18:08 -0700

    Remove unused text and fix exceptions

--- Commands/Github.cs
+++ Commands/Github.cs
@@ -18,25 +18,25 @@ namespace RK800.Commands
     {
         public class Options
         {
-            [Option('t', "tag", Required = false, HelpText = "Tag to download")]
+            [Option('t', "tag", Required = false)]
             public string Tag { get; set; }
 
-            [Option('l', "listags", Required = false, HelpText = "Lists all avaliable tags",  Default = false)]
+            [Option('l', "listags", Required = false, Default = false)]
             public bool ListTags { get; set; }
 
-            [Option('m', "maxtagdisplaylength", Required = false, HelpText = "User name of repsoitory owner.", Default = 12)]
+            [Option('m', "maxtagdisplaylength", Required = false, Default = 12)]
             public int Length { get; set; }
 
-            [Option('p', "prerelease", Required = false, HelpText = "Allow Prereleases?", Default = false)]
+            [Option('p', "prerelease", Required = false, Default = false)]
             public bool AllowPrerelease { get; set; }
 
-            [Option('u', "user", Required = true, HelpText = "User name of repsoitory owner.")]
+            [Value(0, Required = true)]
             public string User { get; set; }
 
-            [Option('r', "repo", Required = true, HelpText = "User name of repsoitory owner.")]
+            [Value(1, Required = true)]
             public string Repo { get; set; }
 
-            [Option('d', "description", Required = false, HelpText = "User name of repsoitory owner.", Default = false)]
+            [Option('d', "description", Required = false, Default = false)]
             public bool Desc { get; set; }
         }
 
@@ -45,126 +45,127 @@ namespace RK800.Commands
         [Summary("Gets a release from the specificed Github repository.\nAvaliable options:\n--repo, -r=string\n--user, -u=string\n--tag, -t=string (default: null)\n--description, -d=bool (default: false)\n--prerelease, -p=bool (default: false)\n--maxtagdisplaylength, -n=int (defualt: 12)")]
         public async Task GetRelease(params string[] Options)
         {
+
             await Context.Channel.TriggerTypingAsync();
             Parser.Default.ParseArguments<Options>(Options)
-            .WithParsed(async o =>
+            //Should prevent any exceptions from breaking the bot
+            .WithParsed(o => _ = GetReleaseTask(o))
+            .WithNotParsed(e => _ = Error.SendDiscordError(Context, Value: "Invalid arguments"));
+        }
+
+        private async Task GetReleaseTask(Options o)
+        {
+            GitHubClient client = new GitHubClient(new ProductHeaderValue("Github"));
+            IReadOnlyList<Release> releases;
+            Repository repo;
+            try
+            {
+                releases = await client.Repository.Release.GetAll(o.User, o.Repo);
+                repo = await client.Repository.Get(o.User, o.Repo);
+            }
+            catch (ApiException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound) await Error.SendDiscordError(Context, Value: "Repository does not exist.");
+                else await Error.SendDiscordError(Context, Value: "Command failed: error reported!", e: e, et: Error.ExceptionType.Fatal);
+                return;
+            }
+
+            if (releases.Count == 0)
+            {
+                await Error.SendDiscordError(Context, Value: "No Releases have been made!");
+                return;
+            }
+
+            if (o.ListTags)
+            {
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.WithColor(Color.Blue);
+                embed.WithTitle("Tags");
+                embed.Description = string.Empty;
+                for (int i = 0; i < releases.Count; i++)
                 {
-                    GitHubClient client = new GitHubClient(new ProductHeaderValue("Github"));
-                    IReadOnlyList<Release> releases;
-                    Repository repo;
+                    Release release = releases[i];
+                    embed.Description += $"{release.TagName}\n";
+                    if (i == o.Length - 1)
+                    {
+                        embed.Description += $"{releases.Count - o.Length} more...";
+                        break;
+                    }
+                }
+                await ReplyAsync(embed: embed.Build());
+                return;
+            }
+
+            Release tag;
+            if (o.Tag != null)
+            {
+                try
+                {
+                    tag = releases.Single(x => x.TagName.ToLower() == o.Tag.ToLower());
+                }
+                catch
+                {
+                    await Error.SendDiscordError(Context, Value: "A release with that tag was not found!");
+                    return;
+                }
+            }
+            else
+            {
+                if (o.AllowPrerelease) tag = releases.First();
+                else
+                {
                     try
                     {
-                        releases = await client.Repository.Release.GetAll(o.User, o.Repo);
-                        repo = await client.Repository.Get(o.User, o.Repo);
+                        tag = releases.First(x => x.Prerelease == o.AllowPrerelease);
                     }
-                    catch (ApiException e)
+                    catch
                     {
-                        if (e.StatusCode == HttpStatusCode.NotFound) await Error.SendDiscordError(Context, Value: "Repository does not exist.");
-                        else await Error.SendDiscordError(Context, Value: "Command failed: error reported!", e: e, et: Error.ExceptionType.Fatal);
+                        await Error.SendDiscordError(Context, Value: "All releases are Pre-releases!");
                         return;
                     }
+                }
+            }
 
-                    if (releases.Count == 0)
-                    {
-                        await Error.SendDiscordError(Context, Value: "No Releases have been made!");
-                        return;
-                    }
-
-                    if (o.ListTags)
-                    {
-                        EmbedBuilder embed = new EmbedBuilder();
-                        embed.WithColor(Color.Blue);
-                        embed.WithTitle("Tags");
-                        embed.Description = string.Empty;
-                        for (int i = 0; i < releases.Count; i++)
-                        {
-                            Release release = releases[i];
-                            embed.Description += $"{release.TagName}\n";
-                            if (i == o.Length - 1)
-                            {
-                                embed.Description += $"{releases.Count - o.Length} more...";
-                                break;
-                            }
-                        }
-                        await ReplyAsync(embed: embed.Build());
-                        return;
-                    }
-
-                    Release tag;
-                    if (o.Tag != null)
-                    {
-                        try
-                        {
-                            tag = releases.Single(x => x.TagName.ToLower() == o.Tag.ToLower());
-                        }
-                        catch
-                        {
-                            await Error.SendDiscordError(Context, Value: "A release with that tag was not found!");
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        if (o.AllowPrerelease) tag = releases.First();
-                        else
-                        {
-                            try
-                            {
-                                tag = releases.First(x => x.Prerelease == o.AllowPrerelease);
-                            }
-                            catch
-                            {
-                                await Error.SendDiscordError(Context, Value: "All releases are Pre-releases!");
-                                return;
-                            }
-                        }
-                    }
-
-                    EmbedBuilder builder = new EmbedBuilder();
-                    builder.WithTitle(tag.Name);
-                    builder.WithColor(Color.Blue);
-                    builder.WithUrl(tag.HtmlUrl);
-                    if (o.Desc)
-                    {
-                        if (tag.Body.Length > EmbedBuilder.MaxDescriptionLength)
-                        {
-                            string[] msgs = Misc.ConvertToDiscordSendable(tag.Body, EmbedBuilder.MaxDescriptionLength);
-                            for (int i = 0; i < msgs.Length; i++)
-                            {
-                                string msg = msgs[i];
-                                builder.Description = msg;
-                                if (msgs.Length - 1 == i)
-                                {
-                                    builder.WithCurrentTimestamp();
-
-                                    foreach (ReleaseAsset asset in tag.Assets)
-                                    {
-                                        builder.AddField(asset.Name, $"[Download]({asset.BrowserDownloadUrl})");
-                                    }
-                                }
-                                await ReplyAsync(embed: builder.Build());
-                                if (i == 0)
-                                {
-                                    builder.Title = null;
-                                    builder.Url = null;
-                                }
-                            }
-                            return;
-                        }
-                        builder.WithDescription(tag.Body);
-                    }
-
-                    foreach (ReleaseAsset asset in tag.Assets)
-                    {
-                        builder.AddField(asset.Name, $"[Download]({asset.BrowserDownloadUrl})");
-                    }
-                    await ReplyAsync(embed: builder.Build());
-                    return;
-                })
-                .WithNotParsed(async e =>
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle(tag.Name);
+            builder.WithColor(Color.Blue);
+            builder.WithUrl(tag.HtmlUrl);
+            if (o.Desc)
             {
-                await Error.SendDiscordError(Context, Value: "Invalid arguments");
-            });
+                if (tag.Body.Length > EmbedBuilder.MaxDescriptionLength)
+                {
+                    string[] msgs = Misc.ConvertToDiscordSendable(tag.Body, EmbedBuilder.MaxDescriptionLength);
+                    for (int i = 0; i < msgs.Length; i++)
+                    {
+                        string msg = msgs[i];
+                        builder.Description = msg;
+                        if (msgs.Length - 1 == i)
+                        {
+                            builder.WithCurrentTimestamp();
+
+                            foreach (ReleaseAsset asset in tag.Assets)
+                            {
+                                builder.AddField(asset.Name, $"[Download]({asset.BrowserDownloadUrl})");
+                            }
+                        }
+                        await ReplyAsync(embed: builder.Build());
+                        if (i == 0)
+                        {
+                            builder.Title = null;
+                            builder.Url = null;
+                        }
+                    }
+                    return;
+                }
+                builder.WithDescription(tag.Body);
+            }
+
+            foreach (ReleaseAsset asset in tag.Assets)
+            {
+                builder.AddField(asset.Name, $"[Download]({asset.BrowserDownloadUrl})");
+            }
+            await ReplyAsync(embed: builder.Build());
+            return;
         }
     }
 }
