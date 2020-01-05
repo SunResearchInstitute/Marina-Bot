0a79db3f250d530354bc2280b95cf7ec3eba75a8
Sun <gabehttps@gmail.com>
2020-01-04 22:16:24 -0800

    add summary of suggestions and a way to read them

--- Commands/Client.cs
+++ Commands/Client.cs
@@ -9,6 +9,7 @@ namespace Marina.Commands
     public class Client : ModuleBase<SocketCommandContext>
     {
         [Command("Suggest")]
+        [Summary("Send a suggestion for a feature! Please use this command responsibly")]
         public async Task AddSuggestion(params string[] Suggestion)
         {
             if (SaveHandler.BlacklistSave.Data.Contains(Context.User.Id))
@@ -24,6 +25,18 @@ namespace Marina.Commands
 
             SaveHandler.SuggestionsSave.Data.Add(Context.User.Id, string.Join(" ", Suggestion));
             await ReplyAsync("Thanks for the suggestion");
+        }
+
+        [Command("GetSuggestions")]
+        [RequireOwner]
+        public async Task UploadSuggestions()
+        {
+            if (SaveHandler.BlacklistSave.Data.Count == 0)
+            {
+                await Error.SendDiscordError(Context, Value: "No suggestions");
+                return;
+            }
+            await Context.Channel.SendFileAsync(SaveHandler.SuggestionsSave.FileInfo.FullName);
         }
 
         [Command("Shutdown"), Alias("Quit")]
